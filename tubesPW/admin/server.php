4b2ef515e2231c9e4062bafdce6ac29a5225f206
sriastuti <sria7253@gmail.com>
2018-12-25 23:51:54 +0700

    modify

--- tubesPW/admin/server.php
+++ tubesPW/admin/server.php
@@ -22,7 +22,7 @@ else {
 		<meta name="description" content="">
 		<meta name="author" content="">
 
-		<title>SB Admin - Dashboard</title>
+		<title>ADMIN</title>
 
 		<!-- Bootstrap core CSS-->
 		<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -104,7 +104,7 @@ else {
 
 	<nav class="navbar navbar-expand navbar-dark bg-dark static-top">
 
-		<a class="navbar-brand mr-1" href="index.html">Start Bootstrap</a>
+		<a class="navbar-brand mr-1" href="index.html">Halaman Utama</a>
 
 		<button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
 			<i class="fas fa-bars"></i>
